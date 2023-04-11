e3232d9bc301a3e5678a665b8263862ff859ce42
jinwook <spankylove@naver.com>
2023-04-11 16:49:51 +0900

    로그인 훅 추가

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -3,6 +3,16 @@ import { setCookie, destroyCookie, parseCookies } from 'nookies';
 
 const AuthContext = createContext();
 
+// 고객 정보
+// id : 병원 id
+// name : 병원명
+// clinicUse : 클리닉 서비스 사용여부
+const User = {
+    id: 0,
+    name: '이엘치과',
+    clinicUse: false
+};
+
 const signOut = async () => {
 
 };
@@ -12,12 +22,14 @@ function AuthProvider({ children }) {
     // 쿠키 파싱 ( medivalue_web )
     // decompile 
     const cookies = parseCookies();
-
     // console.log( cookies );
 
+    // 로그인 관련 구현
+   
     return (
         <AuthContext.Provider
             value={{
+                User,
                 signOut
             }}
         >
@@ -26,4 +38,4 @@ function AuthProvider({ children }) {
     )
 }
 
-export { AuthProvider };
+export { User, AuthContext, AuthProvider };
--- src/pages/report/index.jsx
+++ src/pages/report/index.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import Layout from "../layouts/Layout";
+import useAuth from "@/src/hooks/useMember";
 
 export default function ReportIndex() {
-  return <div>리포트</div>;
+
+  const { User } = useAuth();
+  const { id, name, clinicUse } = User;
+
+  return <div>리포트 {name}</div>;
 }
 
 ReportIndex.getLayout = function getLayout(page) {
