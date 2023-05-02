c8c7730bee424fb2e2eb2c80080356ab3120b290
jinwook <spankylove@naver.com>
2023-05-02 14:46:40 +0900

    api 예외처리

--- src/components/main/TopNotice.jsx
+++ src/components/main/TopNotice.jsx
@@ -2,44 +2,48 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
 import { Autoplay } from "swiper";
+import { getOption } from "@/src/util/fetchUtil";
 export default function TopNotice() {
   
   const [notice, setNotice] = useState([]);
   
   useEffect(() => {
-    fetch(process.env.NEXT_PUBLIC_MEDI_API + '/clinic/notice')
+    fetch(process.env.NEXT_PUBLIC_MEDI_API + '/clinic/notice', getOption)
       .then((res) => res.json())
-      .then((data) => setNotice(data.data));
+      .then((data) => setNotice(data.data))
+      .catch((err) => console.log(err));
   }, []);
 
-  
-
   return (
-    <VereticalSwiperWrap>
-      <Swiper
-        // initialSlide={1}
-        direction={"vertical"}
-        modules={[Autoplay]}
-        navigation={true}
-        loop={true}
-        spaceBetween={8}
-        slidesPerView={1}
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
-        // onSlideChange={() => console.log("slide change")}
-        // onSwiper={(swiper) => console.log(swiper)}
-      >
-      {
-          notice.map((item, index) => 
-            <SwiperSlide key={index}>
-              <NoticeList>{item.title}</NoticeList>
-            </SwiperSlide>
-          ) 
-      }
-      </Swiper>
-    </VereticalSwiperWrap>
+    <React.Fragment>
+    {notice && (
+      <VereticalSwiperWrap>
+        <Swiper
+          // initialSlide={1}
+          direction={"vertical"}
+          modules={[Autoplay]}
+          navigation={true}
+          loop={true}
+          spaceBetween={8}
+          slidesPerView={1}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          // onSlideChange={() => console.log("slide change")}
+          // onSwiper={(swiper) => console.log(swiper)}
+        >
+        {
+            notice.map((item, index) => 
+              <SwiperSlide key={index}>
+                <NoticeList>{item.title}</NoticeList>
+              </SwiperSlide>
+            ) 
+        }
+        </Swiper>
+      </VereticalSwiperWrap>
+    )}
+    </React.Fragment>
   );
 }
 const VereticalSwiperWrap = styled.div`
